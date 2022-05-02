4d0b7a55a4a90fc468debe29c4adf1f60f5fa57f
Aayush Jha <aayushjha5@gmail.com>
2022-05-02 22:32:07 +0530

    init

---
+++ src/components/brand/import.js
@@ -0,0 +1,14 @@
+// helper file to import all images for Brand jsx
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png';
+import atlassian from '../../assets/atlassian.png';
+import dropbox from '../../assets/dropbox.png';
+import shopify from '../../assets/shopify.png';
+
+export {
+    google,
+    slack,
+    atlassian,
+    dropbox,
+    shopify
+}
